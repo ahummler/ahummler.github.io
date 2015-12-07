9a8db3092ca623334b3a50e797769660ba920066
Arthur <phillysuperbowl@gmail.com>
2015-12-07 16:46:42 -0500

    personal website

---
+++ main.js
@@ -0,0 +1,36 @@
+// call img by id on mouse leave change it back
+
+// function changeOne() {
+// 	document.getElementById("pic1").src= "images/IMG_0122.jpg"
+// }
+
+// function changeBack() {
+// 	document.getElementById("pic1").src="images/IMG_0539.jpg"
+// }
+
+ function changeTwo() {
+ 	document.getElementById("pic2").src= "images/IMG_0411.jpg"
+ }
+
+ function changeBack2() {
+ 	document.getElementById("pic2").src="images/IMG_0456.jpg"
+ }
+function changeThree() {
+ document.getElementById("pic3").src= "images/IMG_0587.jpg"
+ }
+
+function changeBack3() {
+  document.getElementById("pic3").src="images/IMG_0586.jpg"
+}
+function changeFour() {
+	document.getElementById("pic4").src= "images/IMG_0679.jpg"
+}
+
+function changeBack4() {
+	document.getElementById("pic4").src="images/IMG_0680.jpg"
+}
+// $(document).ready(function() {
+// 	$('#pic1').mouseenter(function() {
+// 		(this).src("images/IMG_0539.jpg");
+// 	})
+// })
